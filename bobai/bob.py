00fde70bb4717343a9891e5f78e1701097a03917
sharp119 <cs.aditya58@gmail.com>
2024-06-30 21:25:54 +0530

    queries go through chat_session working

--- bobai/bob.py
+++ bobai/bob.py
@@ -1,8 +1,12 @@
 import google.generativeai as genai
 import os
+import configparser
+import pymysql
+import unicodedata
+
+
 from langchain.llms import GooglePalm
 from langchain.utilities import SQLDatabase
-import pymysql
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_experimental.sql import SQLDatabaseChain
 from langchain.prompts import SemanticSimilarityExampleSelector
@@ -13,7 +17,15 @@ from langchain.chains.sql_database.prompt import PROMPT_SUFFIX, _mysql_prompt
 from langchain.prompts.prompt import PromptTemplate
 
 
-genai.configure(api_key="AIzaSyD6LUlQKWUzKQQ8izqgzcDYmwq76hfxw9o")
+config = configparser.ConfigParser()
+config.read('../env.ini')
+
+
+
+
+genai_api_key = config.get('keys', 'GENAI_API_KEY')
+
+genai.configure(api_key=genai_api_key)
 
 generation_config = {
   "temperature": 1,
@@ -35,14 +47,14 @@ chat_session = model.start_chat(
 )
 
 response = chat_session.send_message("what is the bank timing")
-import unicodedata
 
-api_key = "AIzaSyCf9cu_-3wzg6dFIGTrqKoDVXC6W7A9DH0"
-llm = GooglePalm(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1)
+api_key = config.get('keys', 'GOOGLE_AI_STUDIO')
+
+llm = GooglePalm(google_api_key=api_key, temperature=0.1)
 db_user = "root"
-db_password = "root"
+db_password = ""
 db_host = "localhost"
-db_name = "myBankDB"
+db_name = "mybankdb"
 db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}", sample_rows_in_table_info=3)
 few_shots = [
     {'Question' : "How many t-shirts do we have left for Nike in XS size and white color?",
@@ -80,6 +92,7 @@ group by t_shirt_id) a left join discounts on a.t_shirt_id = discounts.t_shirt_i
 ]
 
 
+
 embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 to_vectorize = [" ".join(example.values()) for example in few_shots]
 vectorstore = Chroma.from_texts(to_vectorize, embeddings, metadatas=few_shots)
@@ -87,3 +100,60 @@ example_selector = SemanticSimilarityExampleSelector(
         vectorstore=vectorstore,
         k=2,
     )
+
+mysql_prompt = """You are a MySQL expert. Given an input question, first create a syntactically correct MySQL query to run, then look at the results of the query and return the answer to the input question.
+    Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per MySQL. You can order the results to return the most informative data in the database.
+    Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in backticks (`) to denote them as delimited identifiers.
+    Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+    Pay attention to use CURDATE() function to get the current date, if the question involves "today".
+    
+    Use the following format:
+    
+    Question: Question here
+    SQLQuery: Query to run with no pre-amble
+    SQLResult: Result of the SQLQuery
+    Answer: Final answer here
+    
+    No pre-amble.
+    """
+
+example_prompt = PromptTemplate(
+        input_variables=["Question", "SQLQuery", "SQLResult","Answer",],
+        template="\nQuestion: {Question}\nSQLQuery: {SQLQuery}\nSQLResult: {SQLResult}\nAnswer: {Answer}",
+    )
+
+few_shot_prompt = FewShotPromptTemplate(
+        example_selector=example_selector,
+        example_prompt=example_prompt,
+        prefix=mysql_prompt,
+        suffix=PROMPT_SUFFIX,
+        input_variables=["input", "table_info", "top_k"], #These variables are used in the prefix and suffix
+    )
+sql_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True, prompt=few_shot_prompt)
+
+
+
+
+
+
+def route_query(query):
+   
+    if "account balance" in query.lower() or "loan" in query.lower():
+        response = sql_chain.run(query)
+    else:
+        response = chat_session.send_message(query)
+    
+    return response
+
+
+# query = "What is my account balance?"
+# response = route_query(query)
+# print(response)
+
+query = "Tell me about the credit card offers."
+response = route_query(query)
+print(response)
+
+query = "What are the working hours of the bank?"
+response = route_query(query)
+print(response)
